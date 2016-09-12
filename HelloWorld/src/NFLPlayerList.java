eba9d3c8dc3c4d2d598c16c97c4c8496e5e2ef7d
Degreef <bdegreef24@gmail.com>
2016-09-11 22:06:51 -0700

    Nfl qb list

---
+++ HelloWorld/src/NFLPlayerList.java
@@ -0,0 +1,59 @@
+
+public class NFLPlayerList {
+
+	public class NFLQuaterback {
+		 
+        
+		public static void main(String[] args) {
+		 
+		           }
+		 
+		          
+		//Players
+		 
+		           String Player1 = "nameOfPlayer";
+		 
+		           String Player2 = "nameOfPlayer";
+		 
+		           String Player3 = "nameOfPlayer";
+		 
+		           String Player4 = "nameOfPlayer";
+		 
+		           String Player5 = "nameOfPlayer";
+		 
+		           String Player6 = "nameOfPlayer";
+		 
+		           String Player7 = "nameOfPlayer";
+		 
+		           String Player8 = "nameOfPlayer";
+		 
+		           String Player9 = "nameOfPlayer";
+		          
+		           String Player10 = "nameOfPlayer";
+		 
+		 
+		          
+		          
+		//Player stats
+		 
+		 
+		int passingYards = 0;
+		          
+		int completions = 0;
+		          
+		int passAttempts = 0;
+		 
+		int compPercentage =0;
+		
+		int td = 0;
+		          
+		int interceptions = 0;
+		          
+		int qbRating = 0;
+		          
+		 
+		          
+		          
+		}
+		 
+}
